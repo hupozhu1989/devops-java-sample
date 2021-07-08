178c26ab6fadb43964da801fa2e9277eabe0b067
weizhenchao <675320027@qq.com>
2021-07-08 10:18:16 +0800

    add:加点日志

--- src/main/java/io/kubesphere/Application.java
+++ src/main/java/io/kubesphere/Application.java
@@ -1,4 +1,4 @@
-package io.kubesphere.devops;
+package io.kubesphere;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/io/kubesphere/devops/HelloWorldController.java
+++ src/main/java/io/kubesphere/devops/HelloWorldController.java
@@ -1,6 +1,10 @@
 package io.kubesphere.devops;
 
 
+import io.kubesphere.config.ServerConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @EnableAutoConfiguration
 public class HelloWorldController {
+    private  final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
+    @Autowired
+    private ServerConfig serverConfig;
 
     @RequestMapping("/")
     public String sayHello() {
+        logger.info("请求来了。。。");
+        return "power of our life...返回地址>>> "+serverConfig.getUrl();
+    }
+
+    public String sayHello2() {
         return "Really appreciate your star, that's the power of our life.";
     }
 }
